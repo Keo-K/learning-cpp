1f2f179e27781e23ac7b83ff50d9d5782257d194
Keo-K <vikkarch@gmail.com>
2021-11-06 02:46:17 -0400

    N/A

---
+++ Taking Input/taking_input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "Enter a Number: " << std::endl;
+    std::cin >> number;
+    std::cout << "Your number was " << number << ".";
+}
