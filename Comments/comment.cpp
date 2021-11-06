780eecde7768faced2942e50d40f0741856555e7
Keo-K <vikkarch@gmail.com>
2021-11-06 02:42:21 -0400

    N/A

---
+++ Comments/comment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    //this is a comment
+    /*
+    This
+    is
+    a
+    multi
+    line
+    comment
+    */
+    std::cout << "Hello World";
+    return 0;
+}
