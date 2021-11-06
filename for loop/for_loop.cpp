1189c441dc394142b496028b3de5154c6a47b95a
Keo-K <vikkarch@gmail.com>
2021-11-06 02:54:35 -0400

    N/A

---
+++ for loop/for_loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    for (int count = 1; count <= 10; count++)
+    {
+        std::cout << count << std::endl;
+    }
+}
---
+++ while loop/while_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x = 10;
+    while (x >= 1)
+    {
+        std::cout << x << std::endl;
+        x -= 1;
+    }
+}
